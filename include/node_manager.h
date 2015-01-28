dcb2afa03b921619c1ed6f17331be1d8bb981c12
thenatefisher <nate.scott.fisher@gmail.com>
2015-01-28 09:52:50 -0800

    change address setting pins to PD6/PD7, update base address to 0xD0 (0x68 7bit)

--- include/node_manager.h
+++ include/node_manager.h
@@ -20,15 +20,15 @@ uint8_t NODE_getId() {
 
     SPCR    = 0x00; // disable SPI
     PCICR   = 0x00; // disable all pin interrupts
-
-    // set PB4/PB3 as inputs (0 == input | 1 == output)
-    DDRB &= 0b11100111; 
+    
+    // set PD6/PD7 as inputs (0 == input | 1 == output)
+    DDRD = 0b00111111; 
 
     // turn off pullup resistor
-    PORTB = 0x00;
+    PORTD = 0x00;
 
     // capture unit position 
-    uint8_t position = (PINB & 0b00011000) >> 3;
+    uint8_t position = (PIND & 0b11000000) >> 6;
 
     return position;
 
