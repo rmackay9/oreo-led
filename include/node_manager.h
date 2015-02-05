21ed309d78a8504bced15195bc0b9e0a7696e402
NateFisherAtLimeLab <nate@lime-lab.com>
2015-02-04 21:17:39 -0800

    update node management utility to store position information

--- include/node_manager.h
+++ include/node_manager.h
@@ -16,21 +16,37 @@
 #ifndef  NODE_MANAGER_H
 #define  NODE_MANAGER_H
 
-uint8_t NODE_getId() {
+// id of first node considered in 'front' of
+// aircraft - used to determine lighting color orientation
+#define _NODE_STATION_FRONT 2
 
-    SPCR    = 0x00; // disable SPI
-    PCICR   = 0x00; // disable all pin interrupts
-    
-    // set PD6/PD7 as inputs (0 == input | 1 == output)
-    DDRD = 0b00111111; 
+// store node station ID once derived; Before determined
+// by hardware pins, the ID is conisdered uninitialized
+#define _NODE_UNINITIALIZED_STATION 255
+static uint8_t _NODE_station = _NODE_UNINITIALIZED_STATION;
 
-    // turn off pullup resistor
-    PORTD = 0x00;
+// return node station ID
+// For SOLO, the ID scheme is zero-indexed, beginning
+// with left-rear node, inreasing counter-clockwise
+static uint8_t NODE_getId() {
 
-    // capture unit position 
-    uint8_t position = (PIND & 0b11000000) >> 6;
+    if (_NODE_station == _NODE_UNINITIALIZED_STATION) {
 
-    return position;
+        SPCR    = 0x00; // disable SPI
+        PCICR   = 0x00; // disable all pin interrupts
+        
+        // set PD6/PD7 as inputs (0 == input | 1 == output)
+        DDRD = 0b00111111; 
+
+        // turn off pullup resistor
+        PORTD = 0x00;
+
+        // capture unit station 
+        _NODE_station = (PIND & 0b11000000) >> 6;
+
+    }
+
+    return _NODE_station;
 
 }
 
